20648152d38014e39f7cbbc32cd55c19e8404409
Drew Schmidt <wrathematics@gmail.com>
2020-02-05 20:45:52 -0500

    add xpose test

---
+++ tests/xpose.r
@@ -0,0 +1,9 @@
+suppressMessages(library(fmlr))
+suppressMessages(library(craze))
+
+x = matrix(as.double(1:30), 10)
+x_fml = as_fmlmat(as_cpumat(x))
+
+test = as.matrix(t(x_fml))
+truth = t(x)
+stopifnot(all.equal(test, truth))
