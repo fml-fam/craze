13ac026dbedddcc4c793b5b179d0dd0e6a86a3f7
Drew Schmidt <wrathematics@gmail.com>
2020-05-29 22:43:45 -0400

    add eigen bindings

---
+++ R/eigen.r
@@ -0,0 +1,46 @@
+#' eigen
+#' 
+#' Eigenvalue decomposition.
+#' 
+#' @details
+#' Symmetric matrices only.
+#' 
+#' @param x
+#' An fmlmat matrix.
+#' @param only.values
+#' Should only the eigenvalues be returned?
+#' @param symmetric,EISPACK
+#' Ignored.
+#' 
+#' @name eigen
+#' @rdname eigen
+NULL
+
+
+
+eigen_fml = function(x, symmetric, only.values=FALSE, EISPACK=FALSE)
+{
+  x_cp = DATA(x)$dupe()
+  val = skeleton_vec(x_cp)
+  
+  if (isTRUE(only.values))
+    linalg_eigen_sym(x_cp, val)
+  else
+  {
+    vec = skeleton_mat(x_cp)
+    linalg_eigen_sym(x_cp, val, vec)
+  }
+  
+  ret = list()
+  ret$values = wrapfml(val$rev())
+  if (!isTRUE(only.values))
+    ret$vectors = wrapfml(vec$rev_cols())
+  
+  ret
+}
+
+
+
+#' @rdname eigen
+#' @export
+setMethod("eigen", signature(x="fmlmat"), eigen_fml)
---
+++ tests/eigen.r
@@ -0,0 +1,19 @@
+suppressMessages(library(stats))
+suppressMessages(library(craze))
+comm.set.seed(1234)
+
+n = 5
+x = crossprod(matrix(runif(n*n), n))
+y = matrix(1.0:5.0, n, 1)
+x_fml = as_fmlmat(as_cpumat(x))
+
+e_test = eigen(x_fml)
+e_truth = eigen(x)
+
+test = as.matrix(e_test$values)
+truth = e_truth$values
+stopifnot(all.equal(test, truth))
+
+test = as.matrix(e_test$vectors)
+truth = e_truth$vectors
+stopifnot(all.equal(test, truth))
