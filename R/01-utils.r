07e4129712b64576b548e63a21a1329aeeca8be6
Drew Schmidt <wrathematics@gmail.com>
2020-05-29 20:38:57 -0400

    add object skeleton creator

---
+++ R/01-utils.r
@@ -0,0 +1,38 @@
+is_cpumat = function(x) inherits(x, "cpumat")
+is_cpuvec = function(x) inherits(x, "cpuvec")
+is_gpuvec = function(x) inherits(x, "gpuvec")
+is_gpumat = function(x) inherits(x, "gpumat")
+is_mpimat = function(x) inherits(x, "mpimat")
+is_fmlmat = function(x) inherits(x, "fmlmat")
+
+
+
+skeleton_mat = function(x)
+{
+  if (is_fmlmat(x))
+    new_skeleton(DATA(x))
+  
+  else if (is_cpumat(x) || is_cpuvec(x))
+    cpumat()
+  
+  else if (is_gpumat(x) || is_gpuvec(x))
+    gpumat(x$get_card())
+  
+  else if (is_mpimat(x))
+    mpimat(x$get_grid())
+}
+
+skeleton_vec = function(x)
+{
+  if (is_fmlmat(x))
+    new_skeleton(DATA(x))
+  
+  else if (is_cpumat(x) || is_cpuvec(x))
+    cpuvec()
+  
+  else if (is_gpumat(x) || is_gpuvec(x))
+    gpuvec(x$get_card())
+  
+  else if (is_mpimat(x))
+    mpimat(x$get_grid())
+}
